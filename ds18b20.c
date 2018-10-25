dcb5ce1760c33a2bf6f15ae021cb400465d2e351
Abdelaziz Moustafa <44143919+AbdelazizMoustafa10m@users.noreply.github.com>
2018-10-25 23:25:05 +0200

    Add files via upload

---
+++ ds18b20.c
@@ -0,0 +1,208 @@
+/*
+ * ds18b20.c
+ *
+ * Created: 9/5/2018 3:47:16 AM
+ *  Author: Abdelaziz
+ */ 
+
+#include "ds18b20.h"
+
+/** 
+ *  @file  <ds18b20.c>
+ *  @brief <Implements the functions defined in the header file.>
+ *
+ *  @author<Eng/Abdelaziz Moustafa Abdelazem> 
+ *  @date  <5-Sept-18>
+ *
+ */
+
+   /* Master pulls Data line low*/
+   void OneWireBusLow(void)
+   {
+	   CLEAR_BIT(ONE_WIRE_OUTREG,ONE_WIRE_DQ_PIN);
+	   SET_BIT(ONE_WIRE_DIRREG,ONE_WIRE_DQ_PIN);
+   }
+   /* Releases the data line and the external pull up Resistance makes it HIGH*/
+   void OneWireBusFree(void)
+   {
+	   CLEAR_BIT(ONE_WIRE_DIRREG,ONE_WIRE_DQ_PIN);
+   }
+   /* Reads the logic data i.e. 0 or 1 present on the DATA line*/
+   uint8 OneWireRead(void)
+   {
+	   return (IS_BIT_SET(ONE_WIRE_INPUTREG,ONE_WIRE_DQ_PIN));
+   }
+   
+   void One_Wire_Write(uint8 byte)
+   {
+	   uint8 counter = 0;
+	   /* Makes the DQ pin Output and Master pulls bus low for sensor presence pulse duration*/
+	   for(counter=0;counter<8;counter++)
+	   {
+		   OneWireBusLow();
+		   _delay_us(Slave_wait_time_till_pres_pulse);
+		   /* Releasing the Bus if the master wants to write 1 on data line and waits 60us write time slot duration*/
+		   if(byte & 0b00000001)
+		   {
+			   OneWireBusFree();
+		   }
+		   _delay_us(Write_Timeslot_Duration);
+		   OneWireBusFree();
+		   _delay_us(Recovery_Time_Delay);
+		   byte=byte>>1;
+	   }
+   }
+   
+   uint8 One_Wire_Read(void)
+   {
+	   uint8 byte=0x00;
+	   uint8 counter = 0;
+	   /* Makes the DQ pin Output and Master pulls bus low for sensor presence pulse duration*/
+	   for(counter=0;counter<8;counter++)
+	   {
+		   byte=byte>>1;
+		   OneWireBusLow();
+		   _delay_us(Slave_wait_time_till_pres_pulse);
+		   /* Release Line after 1us to read from slave*/
+		   OneWireBusFree();
+		   _delay_us(Waiting_Time_before_Read);
+		   /* Start sampling after 15us waiting before reading from sensor */
+		   if(ONE_WIRE_INPUTREG &(1<<ONE_WIRE_DQ_PIN))
+		   byte|=0b10000000;
+		   _delay_us(Read_Timeslot_Duration);
+	   }
+	   return byte;
+   }
+   
+
+uint8 DS18B20_Sensor_Reset(void)
+{
+	/* Master make the DQ pin as an output and then writing low on it for at least 480us to reset pulse*/
+	OneWireBusLow();
+	_delay_us(Bus_Low_Delay_Time);
+    /* Master releases the bus and then wait at least 60us until slave can send a presence response*/
+	OneWireBusFree();
+	uint8 counter = 0;
+	/* Check if Salve is present or not*/
+	for(counter=0;counter<40;counter++)
+	{
+		if(OneWireRead()==0) 
+		{
+			break;
+		}
+		_delay_us(Slave_wait_time_till_pres_pulse);
+	}
+	if(counter==40)
+	{
+		return	OneWire_NO_ERROR;
+	}
+	
+	for(counter=0;counter<30;counter++)
+	{
+		if(OneWireRead()==1) break;
+		_delay_us(slave_presence_pulse_time);
+	}
+	return OneWire_COMMUINCATION_ERROR;
+	
+}
+
+uint8 ds18b20_scratch_pad[9];
+
+uint8 DS18B20_Sesnsor_Configure(Resolution_Select res)
+ {
+	 /*This Function is used to configure the sensor by selecting  
+	   the resolution and number of sensors on bus*/
+	 
+	 /* Check Communication to know if the sensor is available on the bus or not*/ 
+	 DS18B20_Sensor_Reset();
+	 
+	 /* This line is used to specify that there is only on sensor on the bus*/
+	 One_Wire_Write(Skip_ROM);
+	 
+	 /* This line is used to write Data to the senor*/
+	 One_Wire_Write(Write_Data_to_Scratchpad);
+	 
+	 /* This two lines writes the TH and TL bytes to sensor which are   
+	    garbage value because we don't want to write data to the sensor*/ 
+	 One_Wire_Write(ds18b20_scratch_pad[2]);
+	 One_Wire_Write(ds18b20_scratch_pad[3]);
+	 
+	 /* This line is used to specify the resolution of the sensor*/
+	 One_Wire_Write(res);
+	 
+	 return DS18B20_OK;
+ }
+
+
+
+uint8 DS18B20ReadScratchPad(void)
+{
+	uint8 counter= 0;
+	 /* Check Communication to know if the sensor is available on the bus or not*/
+	DS18B20_Sensor_Reset();
+	/* This line is used to specify that there is only on sensor on the bus*/
+	One_Wire_Write(Skip_ROM);
+	/* This line is used to read Data to the sensor*/
+	One_Wire_Write(Read_Data_From_Scratchpad);
+	/* Reads All 8 bytes of the DS18B20 Sensor Scratchpad*/
+	for(counter=0;counter<8;counter++)
+	{
+		ds18b20_scratch_pad[counter]=One_Wire_Read();
+	}
+	return DS18B20_OK;
+}
+
+uint16 DS18B20ReadTemp(Resolution_Select res)
+{
+	
+	uint16 Temperature = 0;
+	/* Check Communication to know if the sensor is available on the bus or not*/
+	DS18B20_Sensor_Reset();
+	/* This line is used to specify that there is only on sensor on the bus*/
+	One_Wire_Write(Skip_ROM);
+	/* This Line is used to send Temperature conversion request*/
+	One_Wire_Write(Convert_Temp);
+	/* This Switch Case is used to select the waiting conversion time based on resolution*/
+	 switch(res)
+	 {
+		 case Nine_Bit_Resolution:
+		                              _delay_ms(NINE_BIT_RESOLUTION_CONV_TIME);
+		                              break;
+		 case Ten_Bit_Resolution :
+		                              _delay_ms(TEN_BIT_RESOLUTION_CONV_TIME);
+		                              break;
+		 case Eleven_Bit_Resolution:
+		                              _delay_ms(ELVEN_BIT_RESOLUTION_CONV_TIME);
+		 break;
+		 case Tweleve_Bit_Resolution:
+                                   	  _delay_ms(TWELEVE_BIT_RESOLUTION_CONV_TIME);
+		                              break;
+		 default:
+		                              break;
+	 }
+	 /* This Line reads the Scratchpad of the DS18B20 sensor*/
+	 DS18B20ReadScratchPad();
+	 /* Takes the first two bytes only from the Scratchpad because temperature is stored in them*/
+     Temperature= ( (ds18b20_scratch_pad[1]<<8) + ds18b20_scratch_pad[0] );
+	 
+	  /* This line is used to convert the temperature value obtained from the sensor Based on the resolution*/
+	  switch(res)
+	  {
+		  case Nine_Bit_Resolution:
+	                                    return (Temperature*NINE_BIT_RESOLUTION_FACTOR);
+		                                break;
+		  case Ten_Bit_Resolution :
+		                                return (Temperature*TEN_BIT_RESOLUTION_FACTOR);
+		                                break;
+		  case Eleven_Bit_Resolution:
+		                                return (Temperature*ELVEN_BIT_RESOLUTION_FACTOR);
+		                                break;
+		  case Tweleve_Bit_Resolution:
+                                        return (Temperature*TWELEVE_BIT_RESOLUTION_FACTOR);
+		                                break;
+		  default:
+		                                break;
+	  }
+	return DS18B20_OK;
+	
+}
---
+++ ds18b20.h
@@ -0,0 +1,180 @@
+/*
+ * ds18b20.h
+ *
+ * Created: 9/5/2018 3:45:03 AM
+ *  Author: Abdelaziz
+ */ 
+
+
+#ifndef DS18B20_H_
+#define DS18B20_H_
+
+    #include "Micro_Config.h"
+    #include "MACROS.h"
+    #include "STD TYPES.h"
+	
+/** 
+ *  @file  <ds18b20.h>
+ *  @brief <Header File of The DS18B20 temperature sensor library.>
+ *         <It contains The Prototypes of all function available in
+ *          the library,definitions of all Macros, and Enumerations> 
+ *
+ *  @author<Eng/Abdelaziz Moustafa Abdelazem> 
+ *  @date  <5-Sept-18>
+ *
+ */
+	
+	/* Hardware Connection for the sensor*/
+	#define ONE_WIRE_OUTREG    (REG8_Access(PORTC_OUTPUTREG))
+	#define ONE_WIRE_DIRREG    (REG8_Access(PORTC_DIRREG))
+	#define ONE_WIRE_INPUTREG  (REG8_Access(PORTC_INPUTREG))
+	#define ONE_WIRE_DQ_PIN	               PC3
+
+	/* These Macros are the delay time in us needed in the initialization process*/
+	#define Bus_Low_Delay_Time              (500)
+	#define Slave_wait_time_till_pres_pulse (2)
+	#define slave_presence_pulse_time       (10)
+	/*These Macros are the delay time in us needed in the read/write process*/
+	#define Recovery_Time_Delay             (1)
+	#define Waiting_Time_before_Read        (15)
+	#define Write_Timeslot_Duration         (70)
+	#define waiting_Time_before_Write       (15)
+	#define Read_Timeslot_Duration          (60)
+	#define Master_Write_Zero_Duration      (45)
+	#define Master_Read_One_Duration        (45)
+    /* These Macros are the response of the slave when initiating the bus*/
+	#define OneWire_NO_ERROR                (0)
+	#define OneWire_COMMUINCATION_ERROR     (1)
+    /* These Macros are the response of the sensor when trying to Read/write data*/
+    #define DS18B20_COMMUNICATION_ERROR     (1)
+    #define DS18B20_OK                      (0)
+    
+    /* This Macros Define the conversion time in ms for each conversion resolution*/
+    #define NINE_BIT_RESOLUTION_CONV_TIME    (94)
+    #define TEN_BIT_RESOLUTION_CONV_TIME     (188)
+    #define ELVEN_BIT_RESOLUTION_CONV_TIME   (375)
+    #define TWELEVE_BIT_RESOLUTION_CONV_TIME (750)
+    
+    /* This Macros Define The factor in °C for each conversion resolution*/
+    #define NINE_BIT_RESOLUTION_FACTOR    (0.5)
+    #define TEN_BIT_RESOLUTION_FACTOR     (0.25)
+    #define ELVEN_BIT_RESOLUTION_FACTOR   (0.125)
+    #define TWELEVE_BIT_RESOLUTION_FACTOR (0.0625)
+	
+    /* This Enumeration defines the commands sent to the ROM of the sensor to Initiate communication*/
+    typedef enum {Search_ROM = 0xF0, Read_ROM = 0x33, Match_ROM = 0x55,
+	    Skip_ROM = 0xCC, Alarm_Search = 0xEC
+    }ROM_Command;
+    
+	/* This Enumeration defines the commands sent to the sensor to Initiate any process*/
+    typedef enum {Convert_Temp = 0x44, Write_Data_to_Scratchpad = 0x4E, Read_Data_From_Scratchpad = 0xBE,
+	    Copy_Scratchpad_to_EEPROM = 0x48, Recall_Data_from_EEPROM = 0xB8, Read_Power_Supply = 0xB4
+    }DS18B20_Functin_Cmd;
+    
+	/* This Enumeration defines the different resolutions that the sensor can be configured with*/
+    typedef enum {Nine_Bit_Resolution = 0x1F, Ten_Bit_Resolution = 0x3F, Eleven_Bit_Resolution = 0x5F,
+	    Tweleve_Bit_Resolution = 0x7F
+    }Resolution_Select;
+
+    /** 
+	 *  @brief <void OneWireBusLow(void)>
+	 *  <This Function intiates commuincation with one wire protocol by the master pulling bus low>
+	 *
+	 *  @param <void>
+	 *
+	 *  @return <function return is void>
+	 *
+	 */
+    void OneWireBusLow(void);
+	
+	/** 
+	 *  @brief <void OneWireBusFree(void)>
+	 *  <This Function intiates commuincation with one wire protocol by the master floating the bus>
+	 *
+	 *  @param <void>
+	 *
+	 *  @return <function return is void>
+	 *
+	 */
+    void OneWireBusFree(void);
+	
+	/** 
+	 *  @brief <uint8 OneWireRead(void)>
+	 *  <This Function reads the logic data on the bus i.e. 0 or 1 to know if the slave is present on bus>
+	 *
+	 *  @param <void>
+	 *
+	 *  @return <uint8 returns 0 for DS18B20 Prescence on the bus, 1 for non prescence.>
+	 *
+	 */
+    uint8 OneWireRead(void);
+	
+	/** 
+	 *  @brief <void One_Wire_Write(uint8 byte)>
+	 *  <This Function sends one byte via 1-Wire interface to the DS18B20 sensor>
+	 *
+	 *  @param uint8 <byte> <a byte value to be sent to the DS18B20 sensor via 1-wire interface>
+	 *
+	 *  @return <function return is void>
+	 *
+	 */
+    void One_Wire_Write(uint8 byte);
+	
+	/** 
+	 *  @brief <uint8 One_Wire_Read(void)>
+	 *  <This Function reads a byte from the sensor via 1-Wire interface.>
+	 *
+	 *  @param <void>
+	 *
+	 *  @return <uint8 returns byte read from the DS18B20 Sensor via 1-Wire interface.>
+	 *
+	 */
+    uint8 One_Wire_Read(void);
+	
+	/** 
+	 *  @brief <uint8 DS18B20_Sensor_Reset(void)>
+	 *  <This Function Used The function prepares the 1-Wire bus and 
+	 *   initialization of the DS18B20 sensor before data exchange.>
+	 *
+	 *  @param <void>
+	 *
+	 *  @return <uint8 returns 0 for success, 1 for failed initialization attempt.>
+	 *
+	 */
+    uint8 DS18B20_Sensor_Reset(void);
+	
+	/** 
+	 *  @brief <uint8 DS18B20_Sesnsor_Configure(Resolution_Select res)>
+	 *  <This Function Used for configuring the resolution of the sensor.>
+	 *
+	 *  @param Resolution_Select <res> <different resolution from Resolution_Select Enumeration. >
+	 *
+	 *  @return <uint8 returns 0 for success, 1 for failed configuring attempt.>
+	 *
+	 */
+	uint8 DS18B20_Sesnsor_Configure(Resolution_Select res);
+	
+	/** 
+	 *  @brief <uint8 DS18B20ReadScratchPad(void)>
+	 *  <This Function reads the contents of DS18B20 scratchpad.>
+	 *
+	 *  @param <void>
+	 *
+	 *  @return <uint8 returns 0 if CRC error is detected, 1 for successful scratchpad read.>
+	 *
+	 */
+    uint8 DS18B20ReadScratchPad(void);
+	
+	/** 
+	 *  @brief <uint16 DS18B20ReadTemp(Resolution_Select res)>
+	 *  <This functions initiates a temperature conversion via CONVERT_T function command
+	 *   and then issues READ_SCRATCHPAD command for fetching the temperature reading.>
+	 *
+	 *  @param Resolution_Select <res> <different resolution from Resolution_Select Enumeration. >
+	 *
+	 *  @return <uint16 returns Temperature measured by the sensor based on the resolution of conversion.>
+	 *
+	 */
+    uint16 DS18B20ReadTemp(Resolution_Select res);
+
+#endif /* DS18B20_H_ */
